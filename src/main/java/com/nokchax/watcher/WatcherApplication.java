afed2ef6a59986b6de10ad39a115992fad4eb63c
nokchax <nokchax@gmail.com>
2019-09-27 22:44:10 +0900

    load additional property file from external

--- src/main/java/com/nokchax/watcher/WatcherApplication.java
+++ src/main/java/com/nokchax/watcher/WatcherApplication.java
@@ -2,12 +2,20 @@ package com.nokchax.watcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class WatcherApplication {
 
+    private static final String TOKEN_PROPERTY = "file:/data/etc/watcher/token.yml";
+
     public static void main(String[] args) {
         SpringApplication.run(WatcherApplication.class, args);
+
+        new SpringApplicationBuilder()
+                .sources(WatcherApplication.class)
+                .properties("spring.config.additional-location=" + TOKEN_PROPERTY)
+                .run(args);
     }
 
 }
