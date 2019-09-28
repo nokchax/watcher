01f34037767b6537dfd05dd31435aacdded67d4b
nokchax <nokchax@gmail.com>
2019-09-28 14:47:48 +0900

    feat : library import type change and add telegram api initializer

--- src/main/java/com/nokchax/watcher/WatcherApplication.java
+++ src/main/java/com/nokchax/watcher/WatcherApplication.java
@@ -2,6 +2,7 @@ package com.nokchax.watcher;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class WatcherApplication {
@@ -9,6 +10,8 @@ public class WatcherApplication {
     private static final String TOKEN_PROPERTY = "file:/data/etc/watcher/token.yml";
 
     public static void main(String[] args) {
+        ApiContextInitializer.init();
+
         new SpringApplicationBuilder()
                 .sources(WatcherApplication.class)
                 .properties("spring.config.additional-location=" + TOKEN_PROPERTY)
