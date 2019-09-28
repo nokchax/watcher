718605bdfd98b72dea3ffe920a1d7a473271c727
nokchax <nokchax@gmail.com>
2019-09-28 16:08:46 +0900

    feat : add element and element path to express path to get target element

---
+++ src/main/java/com/nokchax/watcher/scrap/domain/Element.java
@@ -0,0 +1,6 @@
+package com.nokchax.watcher.scrap.domain;
+
+public class Element {
+    private String type;//tag, class, name, attribute, etc...
+    private String target;
+}
---
+++ src/main/java/com/nokchax/watcher/scrap/domain/ElementPath.java
@@ -0,0 +1,8 @@
+package com.nokchax.watcher.scrap.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElementPath {
+    List<Element> path = new ArrayList<>();
+}
