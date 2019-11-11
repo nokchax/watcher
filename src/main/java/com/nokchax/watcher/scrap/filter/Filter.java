07c40e867cc23f3060e438100b6d9ee00be64393
nokchax <nokchax@gmail.com>
2019-11-11 22:19:02 +0900

    refactor : method rename

--- src/main/java/com/nokchax/watcher/scrap/filter/Filter.java
+++ src/main/java/com/nokchax/watcher/scrap/filter/Filter.java
@@ -2,5 +2,5 @@ package com.nokchax.watcher.scrap.filter;
 
 public abstract class Filter<T> {
 
-    abstract T filter(Filter<? extends Filter> type);
+    abstract T filtering(Filter<? extends Filter> type);
 }
