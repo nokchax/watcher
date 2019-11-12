1381d7630e15eda45075a8291f0de6d0c48bb7c8
nokchax <nokchax@gmail.com>
2019-11-12 19:31:13 +0900

    comment : about fields necessary

--- src/main/java/com/nokchax/watcher/scrap/filter/Filter.java
+++ src/main/java/com/nokchax/watcher/scrap/filter/Filter.java
@@ -3,7 +3,7 @@ package com.nokchax.watcher.scrap.filter;
 import com.nokchax.watcher.scrap.domain.CompareType;
 
 public abstract class Filter<INPUT, OUTPUT> {
-    protected CompareType compareType;
+    protected CompareType compareType; // is this fields necessary? how about divide one class to several classes
     protected String compareString;
 
     abstract OUTPUT filtering(INPUT input);
