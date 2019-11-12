12ab546494957b9a5fa695f0605edab4c89550f4
nokchax <nokchax@gmail.com>
2019-11-12 19:29:46 +0900

    feat : add necessary fields

--- src/main/java/com/nokchax/watcher/scrap/filter/Filter.java
+++ src/main/java/com/nokchax/watcher/scrap/filter/Filter.java
@@ -1,6 +1,10 @@
 package com.nokchax.watcher.scrap.filter;
 
+import com.nokchax.watcher.scrap.domain.CompareType;
+
 public abstract class Filter<INPUT, OUTPUT> {
+    protected CompareType compareType;
+    protected String compareString;
 
     abstract OUTPUT filtering(INPUT input);
 }
